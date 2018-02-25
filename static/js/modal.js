1c3e2cea810ec77fc2e9e62ba07bd41e3ffae01f
stefanomil <stefano.milizia00@gmail.com>
2018-02-25 13:50:12 +0100

    fix mobile

--- static/js/modal.js
+++ static/js/modal.js
@@ -9,6 +9,7 @@ window.onload = function() {
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
     // When the user clicks the button, open the modal
+
     btn.onclick = function() {
         modal.style.display = "block";
     }
@@ -20,6 +21,22 @@ window.onload = function() {
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    btn.ontap = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.ontap = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.ontap = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
