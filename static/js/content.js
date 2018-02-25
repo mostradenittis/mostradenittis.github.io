3118d11496b1afe91854b7cde91756c217b3d669
gabbiurlaro <gabbiurlaro00@gmail.com>
2018-02-25 10:44:19 +0100

    Stampa sulla console la path che clicco

--- static/js/content.js
+++ static/js/content.js
@@ -1,6 +1,14 @@
 // Script for showing content
 
 function show_content(id){
-  alert("Ingresso")
+  switch (id) {
+    case "ingresso":
+      console.log("Hai cliccato su ingresso");
+      break;
+
+    default:
+      console.log("Non stai cliccando su niente");
+      break;
+  }
   return;
 }
