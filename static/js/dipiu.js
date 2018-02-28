fd69cdda8eeb9e2789dea9d3227311174d9500d2
stefanomil <stefano.milizia00@gmail.com>
2018-02-28 22:53:24 +0100

    more information

---
+++ static/js/dipiu.js
@@ -0,0 +1,7 @@
+function apri_lungo_ofanto() {
+  var dip = document.getElementById("dp_lungo_ofanto");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
