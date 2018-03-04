4c72ecd8e006167b9503f458758300c4b7cfa21b
stefanomil <stefano.milizia00@gmail.com>
2018-03-04 12:44:42 +0100

    added more information about the works

--- static/js/dipiu.js
+++ static/js/dipiu.js
@@ -5,3 +5,63 @@ function apri_lungo_ofanto() {
   else if (dip.style.display=="block")
       dip.style.display="none";
 }
+
+
+function apri_la_senna() {
+  var dip = document.getElementById("dp_la_senna");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
+
+function apri_leontine_in_canotto() {
+  var dip = document.getElementById("dp_leontine_in_canotto");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
+
+function apri_che_freddo() {
+  var dip = document.getElementById("dp_che_freddo");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
+
+function apri_donna_rena() {
+  var dip = document.getElementById("dp_donna_rena");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
+
+
+function apri_sulla_spiaggia() {
+  var dip = document.getElementById("dp_sulla_spiaggia");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
+
+
+function apri_veduta_londra() {
+  var dip = document.getElementById("dp_veduta_londra");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
+
+
+function apri_nubi_westminster() {
+  var dip = document.getElementById("dp_nubi_westminster");
+  if (dip.style.display=="none")
+      dip.style.display="block";
+  else if (dip.style.display=="block")
+      dip.style.display="none";
+}
